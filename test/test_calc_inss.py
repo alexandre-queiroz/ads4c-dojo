e184a11c337189ee6dba0c3cbbb81ce81e40091d
alexandre-queiroz <alexandre.queiroz@aluno.faculdadeimpacta.com.br>
2020-10-26 23:37:39 -0300

    teste valor a pagar

--- test/test_calc_inss.py
+++ test/test_calc_inss.py
@@ -9,3 +9,16 @@ def test_calculo_inss():
 
   # Assert
   assert resultado == expectativa
+
+def test_valor_pagar(salario, enquadramento):
+  # Arrange
+  salario = 0
+  expectativa = 0
+
+  # Act
+  from domain.logic import calculo_inss
+  resultado = calculo_inss(salario)
+
+  # Assert
+  assert resultado == expectativa
+  
